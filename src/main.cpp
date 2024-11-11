5f3b6e24face468969697a5aa16adcebdcefa21f
Innocenzo <innocenzofulginiti@gmail.com>
2024-11-11 15:43:44 +0100

    added probabilistic gate definition to the output QASM file

--- src/main.cpp
+++ src/main.cpp
@@ -38,41 +38,41 @@ int main(int argc, char *argv[]) {
 
     // file_out.close();
 
-    // /* Add probabilistic not gate definition to the output QASM file */
-    // std::ifstream file_out_i(FILENAME_OUT);
-    // std::string line;
-    // std::vector<std::string> lines;
+    /* Add probabilistic gate definition to the output QASM file */
+    std::ifstream file_out_i(FILENAME_OUT);
+    std::string line;
+    std::vector<std::string> lines;
 
-    // // Read the contents of the output file
-    // if (file_out_i.is_open()) {
-    //     while (std::getline(file_out_i, line)) {
-    //         lines.push_back(line);
-    //     }
-    //     file_out_i.close();
-    // }
-    // else {
-    //     std::cerr << "Unable to open file for reading.";
-    //     return 1;
-    // }
+    // Read the contents of the output file
+    if (file_out_i.is_open()) {
+        while (std::getline(file_out_i, line)) {
+            lines.push_back(line);
+        }
+        file_out_i.close();
+    }
+    else {
+        std::cerr << "Unable to open file for reading.";
+        return 1;
+    }
 
-    // // Add the new gate definition after the third line
-    // std::string newGateDefinition = "gate prob_x(prob) q {\n\tx q;\n}";
-    // if (lines.size() >= 4) {
-    //     lines.insert(lines.begin() + 4, newGateDefinition);
-    // }
+    // Add the new gate definition after the third line
+    std::string newGateDefinition = "gate prob_x(prob) q {\n\tx q;\n}";
+    if (lines.size() >= 4) {
+        lines.insert(lines.begin() + 4, newGateDefinition);
+    }
 
-    // // Write the modified contents back to the same file
-    // std::ofstream file_out_o(FILENAME_OUT); // Open the same file for writing
-    // if (file_out_o.is_open()) {
-    //     for (const auto& line : lines) {
-    //         file_out_o << line << std::endl;
-    //     }
-    //     file_out_o.close();
-    // }
-    // else {
-    //     std::cerr << "Unable to open file for writing.";
-    //     return 1;
-    // }
+    // Write the modified contents back to the same file
+    std::ofstream file_out_o(FILENAME_OUT); // Open the same file for writing
+    if (file_out_o.is_open()) {
+        for (const auto& line : lines) {
+            file_out_o << line << std::endl;
+        }
+        file_out_o.close();
+    }
+    else {
+        std::cerr << "Unable to open file for writing.";
+        return 1;
+    }
 
     return 0;
 }
