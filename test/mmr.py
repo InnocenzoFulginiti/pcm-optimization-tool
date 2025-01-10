f631f5c81b989457d9b5b359f8f3f7bcb7484b45
Innocenzo <innocenzofulginiti@gmail.com>
2025-01-10 12:37:46 +0100

    added possibility to tune resets density

--- test/mmr.py
+++ test/mmr.py
@@ -1,4 +1,13 @@
 import numpy as np
+import qiskit.qasm2
+import qiskit.qasm3
+
+from qiskit.transpiler.passes import RemoveResetInZeroState, RemoveFinalReset, RemoveFinalMeasurements, ResetAfterMeasureSimplification
+from qiskit.transpiler import PassManager
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit_ibm_runtime import QiskitRuntimeService
+
+from distutils.util import strtobool
 import argparse
 import sys
 from qiskit.circuit import QuantumRegister, ClassicalRegister, QuantumCircuit
@@ -36,7 +45,7 @@ import os
 import subprocess
 
 def my_random_circuit(
-    num_qubits, depth, max_operands=3, measure=False, conditional=True, reset=False, seed=None, measurements_density=1
+    num_qubits, depth, max_operands=3, measure=False, conditional=True, reset=False, seed=None, measurements_density=8, reset_density=16
 ):
     """Generate random circuit of arbitrary size and form.
 
@@ -69,7 +78,7 @@ def my_random_circuit(
         raise CircuitError("max_operands must be between 1 and 3")
 
     one_q_ops = [
-        #IGate,
+        IGate,
         U1Gate,
         U2Gate,
         U3Gate,
@@ -132,8 +141,8 @@ def my_random_circuit(
         cr = ClassicalRegister(num_qubits, "c")
         qc.add_register(cr)
 
-    if reset:
-        one_q_ops += [Reset]
+    # if reset:
+    #     one_q_ops += [Reset]
 
     if seed is None:
         seed = np.random.randint(0, np.iinfo(np.int32).max)
@@ -146,36 +155,42 @@ def my_random_circuit(
         remaining_qubits = list(range(num_qubits))
         rng.shuffle(remaining_qubits)
         while remaining_qubits:
-            max_possible_operands = min(len(remaining_qubits), max_operands)
-            num_operands = rng.choice(range(max_possible_operands)) + 1
-            operands = [remaining_qubits.pop() for _ in range(num_operands)]
-            if num_operands == 1:
-                operation = rng.choice(one_q_ops)
-            elif num_operands == 2:
-                operation = rng.choice(two_q_ops)
-            elif num_operands == 3:
-                operation = rng.choice(three_q_ops)
-            if operation in one_param:
-                num_angles = 1
-            elif operation in two_param:
-                num_angles = 2
-            elif operation in three_param:
-                num_angles = 3
+            if reset and rng.choice(range(reset_density)) == 0:
+                operands = [remaining_qubits.pop()]
+                angles = []
+                register_operands = [qr[i] for i in operands]
+                op = operation(*angles)
             else:
-                num_angles = 0
-            angles = [rng.uniform(0, 2 * np.pi) for x in range(num_angles)]
-            register_operands = [qr[i] for i in operands]
-            op = operation(*angles)
+                max_possible_operands = min(len(remaining_qubits), max_operands)
+                num_operands = rng.choice(range(max_possible_operands)) + 1
+                operands = [remaining_qubits.pop() for _ in range(num_operands)]
+                if num_operands == 1:
+                    operation = rng.choice(one_q_ops)
+                elif num_operands == 2:
+                    operation = rng.choice(two_q_ops)
+                elif num_operands == 3:
+                    operation = rng.choice(three_q_ops)
+                if operation in one_param:
+                    num_angles = 1
+                elif operation in two_param:
+                    num_angles = 2
+                elif operation in three_param:
+                    num_angles = 3
+                else:
+                    num_angles = 0
+                angles = [rng.uniform(0, 2 * np.pi) for x in range(num_angles)]
+                register_operands = [qr[i] for i in operands]
+                op = operation(*angles)
 
-            # with some low probability, condition on classical bit values
-            if conditional and rng.choice(range(measurements_density)) == 0 and len(remaining_qubits) > 0 and num_operands == 1:
-                value = 1 # Condition value for the classical controlled operation
-                
-                measured_qubit = remaining_qubits.pop()
-                
-                qc.measure(qr[measured_qubit], cr_i[measured_qubit])
-                
-                op = op.c_if(cr_i[measured_qubit], 1)
+                # with some low probability, condition on classical bit values
+                if conditional and rng.choice(range(measurements_density)) == 0 and len(remaining_qubits) > 0 and num_operands == 1:
+                    value = 1 # Condition value for the classical controlled operation
+                    
+                    measured_qubit = remaining_qubits.pop()
+                    
+                    qc.measure(qr[measured_qubit], cr_i[measured_qubit])
+                    
+                    op = op.c_if(cr_i[measured_qubit], value)
                 
             qc.append(op, register_operands)
 
@@ -184,18 +199,32 @@ def my_random_circuit(
 
     return qc
 
-def generate_circuits(num_qubits, depth, density, num_circuits, measure, reset, seed, max_operands):
+def generate_circuits(num_qubits, depth, measurement_density, reset_density, num_circuits, measure, reset, seed, max_operands, pass_manager=None):
     dataset = []
+    dataset_post = []
+    post_pass_manager = PassManager()
+    post_pass_manager.append(RemoveResetInZeroState())
+    # post_pass_manager.append(ResetAfterMeasureSimplification())
+
     for i in range(num_circuits):
-        dataset.append(my_random_circuit(num_qubits=num_qubits, depth=depth, max_operands=max_operands,
-                    measure=measure, reset=reset, seed=seed, measurements_density=density).qasm())
-    return dataset
+        qc = my_random_circuit(num_qubits=num_qubits, depth=depth, max_operands=max_operands, measure=measure, reset=reset, seed=seed, measurements_density=measurement_density, reset_density=reset_density)
+        if pass_manager != None:
+            qc = pass_manager.run(qc)
+
+        qc_post = post_pass_manager.run(qc)
+        print(f"Generated circuit #{i}")
+
+        dataset.append(qiskit.qasm2.dumps(qc))
+        dataset_post.append(qiskit.qasm2.dumps(qc_post))
+        
+    return [dataset, dataset_post]
 
 # Function to count the total number of measurements and probabilisti cates in the files of a given folder
-def count_measure_operations(folder):
+def count_measure_reset_operations(folder, delete_file=False):
     measure_count = 0
     prob_gate_count = 0
     num_circuits = 0
+    reset_count = 0
     
     for filename in os.listdir(folder):
         file_path = os.path.join(folder, filename)
@@ -209,17 +238,22 @@ def count_measure_operations(folder):
             for line in file:
                 measure_count += line.count("measure")
                 prob_gate_count += line.count("prob_")
+                reset_count += line.count("reset")
+
+        if delete_file:
+            os.remove(file_path)
     
-    return [measure_count, prob_gate_count, num_circuits]
+    return [measure_count, prob_gate_count, reset_count, num_circuits]
 
 def main():
+    # Parse command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("num_circuits", type=int)
     parser.add_argument("num_qubits", type=int)
     parser.add_argument("depth", type=int)
-    parser.add_argument("density", type=int)
-    parser.add_argument("reset", type=bool)
-    
+    parser.add_argument("meas_density", type=int)
+    parser.add_argument("reset", type=lambda x: bool(strtobool(x)))
+    parser.add_argument("res_density", type=int)
     try:
         args = parser.parse_args()
     except SystemExit as e:
@@ -230,31 +264,56 @@ def main():
     num_circuits = args.num_circuits
     num_qubits = args.num_qubits
     depth = args.depth
-    density = args.density
+    meas_density = args.meas_density
     reset = args.reset
+    res_density = args.res_density
 
     max_operands = 3
-    measure = True
+    measure = False
     seed = None
 
+    # Pass manager initialization
+    pass_manager = None
+    use_pass_manager = False
+    if use_pass_manager:
+        backend_name = "ibm_sherbrooke"
+        service = QiskitRuntimeService()
+        backend = service.backend(backend_name)
+        pass_manager = generate_preset_pass_manager(backend=backend, optimization_level=3)
+    if pass_manager is not None:
+        print("Pass manager initialized")
+
+    # Flag to generate another directory where circuits are optimized using qiskit passes:
+    # RemoveResetInZeroState, RemoveFinalReset, RemoveFinalMeasurements
+    generate_qiskit_opt = True
+
     cpp_program = "../build/qcprop_main"
+    
+    non_opt_folder = f"./non_opt_circuits_n_{num_qubits}_dp_{depth}_dn_{meas_density}_res_{reset}_res_dn_{res_density}"
+    mmr_opt_folder = f"./mmr_opt_circuits_n_{num_qubits}_dp_{depth}_dn_{meas_density}_res_{reset}_res_dn_{res_density}"
 
-    input_folder = f"./input_circuits_n_{num_qubits}_dp_{depth}_dn_{density}_res_{reset}"
-    output_folder = f"./output_circuits_n_{num_qubits}_dp_{depth}_dn_{density}_res_{reset}"
+    os.makedirs(non_opt_folder, exist_ok=True)
+    os.makedirs(mmr_opt_folder, exist_ok=True)
 
-    os.makedirs(input_folder, exist_ok=True)
-    os.makedirs(output_folder, exist_ok=True)
+    [generated_circuits_non_opt, generated_circuits_qiskit_opt] = generate_circuits(num_qubits=num_qubits, depth=depth, num_circuits=num_circuits, 
+        meas_density=meas_density, res_density=res_density, reset=reset, max_operands=max_operands, seed=seed, measure=measure, pass_manager=pass_manager)
 
-    generated_circuits = generate_circuits(num_qubits=num_qubits, depth=depth, num_circuits=num_circuits, 
-        density=density, reset=reset, max_operands=max_operands, seed=seed, measure=measure)
+    folder_qiskit_opt = ""
+    if generate_qiskit_opt:
+        folder_qiskit_opt = f"./qiskit_opt_circuits_n_{num_qubits}_dp_{depth}_dn_{meas_density}_res_{reset}_res_dn_{res_density}"
+        os.makedirs(folder_qiskit_opt, exist_ok=True)
+        for i, circuit in enumerate(generated_circuits_qiskit_opt):
+            filename = os.path.join(folder_qiskit_opt, f"qiskit_opt_{i}.qasm")
+            with open(filename, 'w') as file:
+                file.write(circuit + "\n")
 
-    for i, circuit in enumerate(generated_circuits):
-        filename = os.path.join(input_folder, f"c_{i}.qasm")
+    for i, circuit in enumerate(generated_circuits_non_opt):
+        filename = os.path.join(non_opt_folder, f"non_opt_{i}.qasm")
         with open(filename, 'w') as file:
-            file.write(circuit)
+            file.write(circuit + "\n")
 
-    for input_filename in os.listdir(input_folder):
-        input_file_path = os.path.join(input_folder, input_filename)
+    for input_filename in os.listdir(non_opt_folder):
+        input_file_path = os.path.join(non_opt_folder, input_filename)
         
         # Don't consider folders
         if not os.path.isfile(input_file_path):
@@ -264,8 +323,8 @@ def main():
             continue
         
         # Generate the output filename and path
-        output_filename = f"output_{input_filename}"
-        output_file_path = os.path.join(output_folder, output_filename)
+        output_filename = f"mmr_opt_{input_filename}"
+        output_file_path = os.path.join(mmr_opt_folder, output_filename)
         
         # Run the C++ program with the input and output paths as arguments
         try:
@@ -274,23 +333,59 @@ def main():
         except subprocess.CalledProcessError as e:
             print(f"\033[91mError processing {input_filename}: {e}\033[0m")
         
-    [input_measurements, _, input_num_circuits] = count_measure_operations(input_folder)
-    [output_measurements, output_prob_gates, output_num_circuits] = count_measure_operations(output_folder)
+    [non_opt_measurements, _, non_opt_resets, non_opt_num_circuits] = count_measure_reset_operations(non_opt_folder)
+    [mmr_opt_measurements, mmr_opt_prob_gates, mmr_opt_resets, mmr_opt_num_circuits] = count_measure_reset_operations(mmr_opt_folder)
 
-    print("###############################")
-    print(f"Total measurements in input folder: {input_measurements}")
-    print(f"Num circuits in input folder: {input_num_circuits}")
-    print(f"Average measurement operations (inputs): {input_measurements / input_num_circuits}")
-    print("-----------------")
-    print(f"Total measurements in output folder: {output_measurements}")
-    print(f"Num circuits in output folder: {output_num_circuits}")
-    print(f"Average measurement operations (outputs): {output_measurements / output_num_circuits}")
-    print("-----------------")
+    qiskit_opt_measurements = -1
+    qiskit_opt_resets = -1
+    qiskit_opt_num_circuits = -1
 
-    if input_measurements != 0:
-        print(f"Measurement reduction: {100 - output_measurements * 100 / input_measurements}%")
-        print(f"Number of measuremnts removed: {input_measurements - output_measurements}")
-        print(f"Total probabilistic gates in output folder: {output_prob_gates}")
+    if generate_qiskit_opt:
+        [qiskit_opt_measurements, _, qiskit_opt_resets, qiskit_opt_num_circuits] = count_measure_reset_operations(folder_qiskit_opt)
+    
+    report_file_name = f"report_n_{num_qubits}_dp_{depth}_dn_{meas_density}_res_{reset}_res_dn_{res_density}.txt"
+    report_file_path = os.path.join(non_opt_folder, report_file_name)
+
+    with open(report_file_path, "w") as file:
+        file.write(f"Num circuits in non-opt folder: {non_opt_num_circuits}\n")
+        file.write(f"Total measurements: {non_opt_measurements}\n")
+        file.write(f"Total resets: {non_opt_resets}\n")
+        file.write(f"Average measurement operations: {non_opt_measurements / non_opt_num_circuits}\n")
+        file.write("-------\n")
+        file.write(f"Num circuits in mmr-opt folder: {mmr_opt_num_circuits}\n")
+        file.write(f"Total measurements: {mmr_opt_measurements}\n")
+        file.write(f"Total resets: {mmr_opt_resets}\n")
+        file.write(f"Average measurement operations: {mmr_opt_measurements / mmr_opt_num_circuits}\n")
+        file.write(f"Probabilistic gates: {mmr_opt_prob_gates}\n")
+        file.write("-------\n")
+        if generate_qiskit_opt:
+            file.write(f"Num circuits in qiskit-opt folder: {qiskit_opt_num_circuits}\n")
+            file.write(f"Total measurements: {qiskit_opt_measurements}\n")
+            file.write(f"Total resets: {qiskit_opt_resets}\n")
+            file.write(f"Average measurement operations: {qiskit_opt_measurements / qiskit_opt_num_circuits}\n")
+            file.write("-------\n")
+            file.write("-------\n")
+            file.write("Comparison non-optimized <-> qiskit passes\n")
+            if non_opt_measurements != 0:
+                file.write("-------\n")
+                file.write(f"Measurement reduction: {100 - qiskit_opt_measurements * 100 / non_opt_measurements}%\n")
+                file.write(f"Number of measurements removed: {non_opt_measurements - qiskit_opt_measurements}\n") 
+            if non_opt_resets != 0:
+                file.write("-------\n")
+                file.write(f"Reset reduction: {100 - qiskit_opt_resets * 100 / non_opt_resets}%\n")
+                file.write(f"Number of resets removed: {non_opt_resets - qiskit_opt_resets}\n")
+        
+        file.write("-------\n")
+        file.write("-------\n")
+        file.write("Comparison non-optimized <-> mmr\n")
+        if non_opt_measurements != 0:
+            file.write("-------\n")
+            file.write(f"Measurement reduction: {100 - mmr_opt_measurements * 100 / non_opt_measurements}%\n")
+            file.write(f"Number of measurements removed: {non_opt_measurements - mmr_opt_measurements}\n") 
+        if non_opt_resets != 0:
+            file.write("-------\n")
+            file.write(f"Reset reduction: {100 - mmr_opt_resets * 100 / non_opt_resets}%\n")
+            file.write(f"Number of resets removed: {non_opt_resets - mmr_opt_resets}\n")
 
 
 if __name__ == "__main__":
