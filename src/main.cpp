d16b2e8269a025a61353569ab942297732743931
Innocenzo <innocenzofulginiti@gmail.com>
2025-02-11 12:37:43 +0100

    final version of the main file

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <chrono>
 #include "../include/QuantumComputation.hpp"
 #include "../include/ConstantPropagation.hpp"
 
@@ -13,66 +14,31 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     // Check if exactly two file paths were provided
-    if (argc != 3) {
-        cerr << "Usage: " << argv[0] << " <input_file_path> <output_file_path>" << endl;
+    if (argc != 4) {
+        cerr << "Usage: " << argv[0] << " <input_file_path> <output_file_path> <max_ent_group_size>" << endl;
         return 1;
     }
 
-    // std::string FILENAME_IN = "../test/circuits/circ2.qasm";
-    // std::string FILENAME_OUT = "../test/circuits/circ2_OUT.qasm";
     std::string FILENAME_IN = argv[1];
     std::string FILENAME_OUT = argv[2];
+
+    size_t max_ent_group_size = std::stoull(argv[3]);
 
     qc::QuantumComputation qc(FILENAME_IN);
     qc.unifyQuantumRegisters("q");
 
-    //Print the circuit
-    qc.print(std::cout);
+    // Print the circuit
+    // qc.print(std::cout);
 
-    ConstantPropagation::optimize(qc);
+    ConstantPropagation::optimize(qc, MAX_AMPLITUDES, max_ent_group_size);
 
+    // Print the optimized circuit
     // qc.print(std::cout);
 
     std::ofstream file_out(FILENAME_OUT);
     qc.dumpOpenQASM(file_out);
 
-    // file_out.close();
-
-    /* Add probabilistic gate definition to the output QASM file */
-    std::ifstream file_out_i(FILENAME_OUT);
-    std::string line;
-    std::vector<std::string> lines;
-
-    // Read the contents of the output file
-    /*if (file_out_i.is_open()) {
-        while (std::getline(file_out_i, line)) {
-            lines.push_back(line);
-        }
-        file_out_i.close();
-    }
-    else {
-        std::cerr << "Unable to open file for reading.";
-        return 1;
-    }
-
-    // Add the new gate definition after the third line
-    std::string newGateDefinition = "gate prob_x(prob) q {\n\tx q;\n}";
-    if (lines.size() >= 4) {
-        lines.insert(lines.begin() + 4, newGateDefinition);
-    }
-
-    // Write the modified contents back to the same file
-    std::ofstream file_out_o(FILENAME_OUT); // Open the same file for writing
-    if (file_out_o.is_open()) {
-        for (const auto& line : lines) {
-            file_out_o << line << std::endl;
-        }
-        file_out_o.close();
-    }
-    else {
-        std::cerr << "Unable to open file for writing.";
-        return 1;
-    }*/
+    file_out.close();
 
     return 0;
 }
